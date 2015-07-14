a10aaf1a91f4a104d81b22068c887c1579e87263
littleboron <littleboron@gmail.com>
2015-07-14 15:12:06 +0800

    add

--- src/android/ExtraInfo.java
+++ src/android/ExtraInfo.java
@@ -1,17 +1,26 @@
+package com.lazyathome.cordova;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.app.Activity;
+import android.content.Intent;
 
 public class ExtraInfo extends CordovaPlugin {
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
-                                                                                    throws JSONException {
-                                                                                        Activity activity =this.cordova.getActivity();
-                                                                                if(action.equals("getExtra")) {                                                                 
-                                                                                    Intent i = activity.getIntent();                                                                 
-                                                                                    if(i.hasExtra(Intent.EXTRA_TEXT)) {                                                                 
-                                                                                        callbackContext.success(i.getStringExtra(Intent.EXTRA_TEXT));                                                                 
-                                                                                    }else{                                                                 
-                                                                                        callbackContext.error("");                                                                 
-                                                                                    }                                                                 
-                                                                                    returntrue;                                                                 
-                                                                                }                                                                 
-                                                                                returnfalse;                                                                 
-                                                                            }                                                                 
-                                                                        }                                                                 
+    public boolean execute(String action, JSONArray args,
+            CallbackContext callbackContext) throws JSONException {
+        Activity activity = this.cordova.getActivity();
+        if (action.equals("getExtra")) {
+            Intent i = activity.getIntent();
+            if (i.hasExtra(Intent.EXTRA_TEXT)) {
+                callbackContext.success(i.getStringExtra(Intent.EXTRA_TEXT));
+            } else {
+                callbackContext.error("");
+            }
+            return true;
+        }
+        return false;
+    }
+}
