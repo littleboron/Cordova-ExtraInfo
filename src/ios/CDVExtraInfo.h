289f8339c3384109f2196fa74b0b81ddcf042c3c
littleboron <littleboron@gmail.com>
2015-07-15 09:19:52 +0800

    add iOS plugin

---
+++ src/ios/CDVExtraInfo.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+#import <CoreLocation/CLLocationManager.h>
+#import <Cordova/CDVPlugin.h>
+
+@interface CDVExtraInfo
+
+-(void)getExtra:(CDVInvokedUrlCommand*)command;
+@end
+
---
+++ src/ios/CDVExtraInfo.m
@@ -0,0 +1,12 @@
+
+#import "CDVExtraInfo.h"
+@interface CDVExtraInfo(){
+}
+@end
+
+@implementation CDVExtraInfo
+-(void)getExtra:(CDVInvokedUrlCommand*)command{
+
+}
+
+@end
